8ac247fcee2cfc92e8f51f030b6bd371d8342d57
camillamanzo <camillasouthafrica@gmail.com>
2021-09-23 17:40:51 +0200

    added new file

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld />
+    <NewFile />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import NewFile from './components/NewFile.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    NewFile
   }
 }
 </script>
---
+++ src/components/NewFile.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="hello">
+       <h1>My second title.</h1> 
+    </div>
+    
+</template>
+
+<script>
+    export default {
+    name: 'App',
+    }
+</script>
+<style scoped>
+
+</style>
